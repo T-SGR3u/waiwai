fbf6d7f5f288db5be292834f99063a05136a1420
T-SGR3u <vinlandsaga114@gamil.com>
2020-06-08 01:43:43 +0900

    ページネーション修正_投稿日時追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
 
   def index
-    @posts = Post.includes(:user).page(params[:page]).per(4)
+    @posts = Post.includes(:user).page(params[:page]).per(2).order(created_at: :desc)
     @like = Like.new
     @hash = Gmaps4rails.build_markers(@posts) do |post, marker|
       marker.lat post.latitude
