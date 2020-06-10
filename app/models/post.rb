0211f23bff852a22ce015ef73c9eb99a654ec21a
T-SGR3u <vinlandsaga114@gamil.com>
2020-06-11 00:35:55 +0900

    Active_Strage_add_validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 
   has_one_attached :image
+  validate :image_presence
   belongs_to :user
 
   validates :name, :address, :score, presence: true
@@ -9,5 +10,11 @@ class Post < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode
+
+  def image_presence
+    unless image.attached?
+      errors.add(:image ,"を添付してください")
+    end
+  end
 
 end
