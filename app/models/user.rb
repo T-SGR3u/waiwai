56835041acf57153383580cd0e2e4d58d9fcbd9a
T-SGR3u <vinlandsaga114@gamil.com>
2020-07-04 18:20:52 +0900

    詳細ページ（Gmap,コメント機能）

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :posts ,dependent: :destroy
   has_many :likes ,dependent: :destroy
   has_many :liked_posts ,through: :likes, source: :post
+  has_many :comments
 
   validates :name, presence: true
   validates :email, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :posts do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: :create
   end
   root to:"posts#index"
 end
